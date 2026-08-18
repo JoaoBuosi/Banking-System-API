057b849c026f46479dca43bd23eb179a40f6013b
Joao Vitor Fernandes Buosi <joaovbuosi@gmail.com>
2026-08-18 10:19:08 -0300

    Add SaldoInsuficienteException class

---
+++ src/com/bank/exception/SaldoInsuficienteException.java
@@ -0,0 +1,7 @@
+package com.bank.exception;
+
+public class SaldoInsuficienteException extends RuntimeException {
+    public SaldoInsuficienteException(String message) {
+        super(message);
+    }
+}
