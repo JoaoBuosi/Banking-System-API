52c091e6253687140e94387fbf503df2e6ed16e2
Joao Vitor Fernandes Buosi <joaovbuosi@gmail.com>
2026-08-18 10:15:53 -0300

    Add JWT token generation in AuthController

--- src/com/bank/controller/AuthController.java
+++ src/com/bank/controller/AuthController.java
@@ -1,3 +1,13 @@
+package com.bank.controller;
+
+import com.bank.security.JwtUtil;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
